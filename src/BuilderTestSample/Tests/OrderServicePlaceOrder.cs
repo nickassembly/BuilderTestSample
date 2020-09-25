2f3e11d0c8bfdf47d0c5be9d06a167e61de924e0
Nick Guerra <nickg@maliciousgames.com>
2020-09-25 09:41:54 -0500

    added validation tests

--- src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
+++ src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
@@ -5,20 +5,54 @@ using Xunit;
 
 namespace BuilderTestSample.Tests
 {
-    public class OrderServicePlaceOrder
-    {
-        private readonly OrderService _orderService = new OrderService();
-        private readonly OrderBuilder _orderBuilder = new OrderBuilder();
+   public class OrderServicePlaceOrder
+   {
+      private readonly OrderService _orderService = new OrderService();
+      private readonly OrderBuilder _orderBuilder = new OrderBuilder();
 
-        [Fact]
-        public void ThrowsExceptionGivenOrderWithExistingId()
-        {
-            //var order = _orderBuilder
-            //                .WithTestValues()
-            //                .Id(123)
-            //                .Build();
+      [Fact]
+      public void ThrowsException_GivenOrderWithExistingId()
+      {
+         var order = _orderBuilder
+                         .WithTestValues()
+                         .Id(123)
+                         .Build();
 
-            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
-        }
-    }
+         Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+      }
+
+      [Fact]
+      public void ThrowsException_GivenOrderAmountOfZero()
+      {
+         var order = _orderBuilder
+                         .WithTestValues()
+                         .OrderAmount(0)
+                         .Build();
+
+         Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+      }
+
+      [Fact]
+      public void ThrowsException_GivenOrderWithoutCustomer()
+      {
+         var order = _orderBuilder
+                         .WithTestValues()
+                         .Customer(null)
+                         .Build();
+
+         Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+      }
+
+      [Fact]
+      public void ThrowsException_GivenCustomerWithIdZero()
+      {
+         var order = _orderBuilder
+                         .WithTestValues()
+                         .Customer(new Model.Customer(0))
+                         .Build();
+
+         Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
+      }
+
+   }
 }
--- src/BuilderTestSample/Tests/TestBuilders/OrderBuilder.cs
+++ src/BuilderTestSample/Tests/TestBuilders/OrderBuilder.cs
@@ -1,34 +1,49 @@
 ﻿using BuilderTestSample.Model;
+using System;
 
 namespace BuilderTestSample.Tests.TestBuilders
 {
-    /// <summary>
-    /// Reference: https://ardalis.com/improve-tests-with-the-builder-pattern-for-test-data
-    /// </summary>
-    public class OrderBuilder
-    {
-        private Order _order = new Order();
+   /// <summary>
+   /// Reference: https://ardalis.com/improve-tests-with-the-builder-pattern-for-test-data
+   /// </summary>
+   public class OrderBuilder
+   {
+      private Order _order = new Order();
 
-        public OrderBuilder Id(int id)
-        {
-            _order.Id = id;
-            return this;
-        }
+      public OrderBuilder Id(int id)
+      {
+         _order.Id = id;
+         return this;
+      }
 
-        public Order Build()
-        {
-            return _order;
-        }
+      public Order Build()
+      {
+         return _order;
+      }
 
-        public OrderBuilder WithTestValues()
-        {
-            _order.TotalAmount = 100m;
+      public OrderBuilder WithTestValues()
+      {
+         _order.TotalAmount = 100m;
 
-            // TODO: replace next lines with a CustomerBuilder you create
-            // _order.Customer = new Customer();
-            // _order.Customer.HomeAddress = new Address();
+         // TODO: replace next lines with a CustomerBuilder you create
+         // _order.Customer = new Customer();
+         // _order.Customer.HomeAddress = new Address();
 
-            return this;
-        }
-    }
+         return this;
+      }
+
+      public OrderBuilder OrderAmount(decimal amount)
+      {
+         _order.TotalAmount = amount;
+         return this;
+      }
+
+      public OrderBuilder Customer(Customer customer)
+      {
+         _order.Customer = customer;
+         return this;
+      }
+
+
+   }
 }
